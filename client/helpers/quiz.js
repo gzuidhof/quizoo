7d4a7d22a7819bddb800612f3cc8e09395bff4d7
Guido <me@guido.io>
2015-04-22 22:45:29 +0200

    Rename 'home' to base

---
+++ client/helpers/quiz.js
@@ -0,0 +1,19 @@
+Template.registerHelper('quiz', function() {
+     var quizQuestion = {
+       question: "2 woorden 8 letters",
+       answers: [
+        "Duurt lang",
+        "Bier halen",
+        "Duurt lan"
+       ],
+       correctAnswer: 2
+     }
+
+     var quiz = {
+       name: "TestQuizName",
+       questions: [quizQuestion]
+     }
+
+     return quiz;
+
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,27 @@
+
+Router.route('/', {
+  name: '/home',
+  action: function () {
+    this.render('base');
+    this.render('home');
+  }
+});
+
 Router.route('/login/', {
   name: '/login',
   action: function () {
-    this.render('home');
+    this.render('base');
     this.render('login');
   }
 });
+
+
 
 // we want to be sure that the user is logging in
 // for all routes but login
 Router.onBeforeAction(function () {
     if (!Meteor.user() && !Meteor.loggingIn()) {
+      console.log("awrooooo");
         this.redirect('/login');
     } else {
         // required by Iron to process the route handler
@@ -22,13 +34,21 @@ Router.onBeforeAction(function () {
 Router.route('/lobby', {
   name: '/lobby',
   action: function () {
-    this.render('home');
+    this.render('base');
     this.render('lobby');
   }
 });
 // add here other routes
+Router.route('/student/quiz/:_id', {
+  action: function () {
+    this.render('base');
+    this.render('quizstudent');
+  }
+});
+
+
 
 // catchall route
 Router.route('/(.*)', function () {
-    this.redirect('/login');
+    this.redirect('/');
 });
