7cbc30166caaf719a7decae602ccb8b54b5d11b0
Guido <me@guido.io>
2015-05-09 19:42:03 +0200

    Infotiles for dashboard

---
+++ client/views/teacher/widgets/infotiles.js
@@ -0,0 +1,30 @@
+Template.infotiles.helpers({
+  tiles: [
+    {
+      title: "Leerlingen",
+      icon: "fa fa-users",
+      color: "green",
+      value: 16
+    },
+    {
+      title: "Teams",
+      icon: "fa fa-sitemap",
+      color: "red",
+      value: 4
+    },
+    {
+      title: "Quizzes",
+      icon: "fa fa-graduation-cap",
+      color: "orange",
+      value: 17
+    },
+    {
+      title: "Vragen",
+      icon: "fa fa-question-circle",
+      color: "blue",
+      value: 127
+    }
+  ]
+
+
+});
