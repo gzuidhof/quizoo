93598e4b02bc79fdb862131091bc675871addce0
Guido <me@guido.io>
2015-06-03 01:36:52 +0200

    Basic bootstrap login UI

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,25 +1,7 @@
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-	Template.body.helpers({
-	});
-
-	  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
-	Template.body.events({
-	});
-	// Define a helper to check if the current user is the task owner
+  // Define a helper to check if the current user is the task owner
 	Accounts.ui.config({
 	  passwordSignupFields: "USERNAME_ONLY"
 	});
-	Template.loginButtons.rendered = function()
-	{
-		Accounts._loginButtonsSession.set('dropdownVisible', true);
-		$('.login-close-text').remove();
-	};
-}
-
-Meteor.methods({
-});
-
-if (Meteor.isServer) {
 }
